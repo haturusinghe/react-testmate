d90d2aca334cfb35c2800a953ea5b574e3345da6
S.Haturusinghe <tothegoatz@gmail.com>
2019-12-07 11:57:46 +0530

    Populted Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
-    return <div></div>;
+    return (
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Users</Link>
+          </li>
+        </ul>
+      </nav>
+    );
   }
 }
